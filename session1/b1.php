5467134a016532451b6f6d7a14d4aacb3fa92174
kudatnguyen <quocdatphp>
2019-07-04 19:27:45 +0700

    bai 1

---
+++ session1/b1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="style/css.css">
+	<meta charset="utf-8">
+	<title> Javascript book</title>
+</head>
+<body>
+	<h1>Javascript and DOM</h1>
+	<img src="img/img1.png" alt="thoi trang nu">
+	<p class="demo">ABC</p>
+	<p id="welc">welcome</p>
+	<p>Trở về sau khi giành được ngôi vị cao nhất cuộc thi Hoa hậu Phụ nữ toàn thế giới, hoa hậu Dương Thùy Linh ngày càng rạng rỡ và xinh đẹp. Những hình ảnh mới nhất của cô cùng trang phục Elise tiếp tục trở thành tâm điểm và là gợi ý đầy hữu hiệu cho phái đẹp hiện đại.</p>
+	
+</body>
+</html>
---
+++ session1/taoCV/cv.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style/css1.css">
+	<meta charset="utf-8">
+	<title>CV cá nhân</title>
+
+</head>
+<body>
+	<img src="img/img.jpg" alt="ảnh">
+
+	<div class="container">
+		<p> Đt: 0989068 </p>
+		<p> Email: </p>
+		<p> Địa chỉ: 78 Lê Duẩn</p>
+
+		<strong>Thông tin cá nhân</strong>
+		<p>Ngày sinh: </p>
+		<p>Quốc tịch:</p>
+		<p>Cấp bậc:</p>
+		<p>Kinh Nghiệm:</p>
+		<p>Giới tính:</p>
+		<p>Hôn nhân:</p>
+
+		<strong>Mục tiêu nghề nghiệp</strong>
+		<p>tôi đang tìm kiếm một vị trí có tính thử thách cao trong công việc. Tôi yêu thích và không ngại khó khăn</p>
+
+		<strong>Kinh Nghiệm Làm Việc</strong>
+		<h3>Công Ty
+			<ul style="font-weight: initial;">
+				<li>Apple</li>
+			</ul>
+		</h3>
+
+		<h3>Thời gian
+			<ul style="font-weight: initial;">
+				<li>Apple</li>
+			</ul>
+		</h3>
+
+		<h3>chức vụ
+			<ul style="font-weight: initial;">
+				<li>Apple</li>
+			</ul>
+		</h3>
+
+		<h3>Mo tả
+			<ul style="font-weight: initial;">
+				<li>Apple</li>
+			</ul>
+		</h3>
+
+		<strong>Ngôn ngữ</strong>
+		<p>English</p>
+		<p>Frank</p>
+		
+	</div>	
+	
+	
+	
+</body>
+</html>
